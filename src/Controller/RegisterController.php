6831061b6c2135ff2283df44a4dc8cfe8b7cc161
SophieHervieu <sophie-hervieu@live.fr>
2025-03-24 14:04:37 +0100

    exercice 1 validation champs

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Account;
+use App\Repository\AccountRepository;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final class RegisterController extends AbstractController
+{
+    public function __construct(
+        private readonly AccountRepository $accountRepository,
+        private readonly EntityManagerInterface $em
+    ){}
+
+    #[Route('/register', name: 'app_register_addaccount')]
+    public function addAccount(Request $request, ValidatorInterface $validator): Response
+    {
+        $msg = "";
+        $type = "";
+        //Crée un objet Account
+        $account = new Account();
+        //Crée un objet RegisterType(formulaire)
+        $form = $this->createForm(RegisterType::class, $account);
+        //Récupère le résultat de la requête
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            //Si l'entité est valide (validation)
+            $errors = $validator->validate($account);
+            if(count($errors) > 0){
+                $msg = $errors[0]->getMessage();
+                $type = "warning";
+            }
+            //Sinon on ajoute en BDD
+            else {
+                 //Teste si le compte n'existe pas
+                if(!$this->accountRepository->findOneBy(["email" => $account->getEmail()])) {
+                    $account->setRoles(["ROLE_USER"]);
+                    $this->em->persist($account);
+                    $this->em->flush();
+                    $msg = "Le compte a été ajouté en BDD";
+                    $type = "success";
+                }
+                else {
+                    $msg = "Les informations email et/ou password existent déjà";
+                    $type = "danger";
+                }
+            }
+           
+            $this->addFlash($type, $msg);
+        }
+
+        return $this->render('register/addaccount.html.twig', [
+            'form' => $form
+        ]);
+    }
+}
---
+++ src/Entity/Account.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccountRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: AccountRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class Account implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    #[Assert\NotBlank(
+        message:"L'email doit être renseigné"
+    )]
+    #[Assert\Email(
+        message:"L'email doit être valide"
+    )]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    #[Assert\Regex(
+        pattern:'/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/',
+        match: false,
+        message: "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un nombre",
+        )]
+        #[Assert\NotCompromisedPassword]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(
+        message:"Le prénom doit être renseigné"
+    )]
+    #[Assert\Length(
+        min: 2,
+        minMessage: "Le prénom doit faire au moins 2 caractères"
+    )]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(
+        message:"Le nom doit être renseigné"
+    )]
+    #[Assert\Length(
+        min: 2,
+        minMessage: "Le nom doit faire au moins 2 caractères"
+    )]
+    private ?string $lastname = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+}
